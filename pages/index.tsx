a99276fc0500a10c112c2b7d96347abeae3a0391
Graner007 <bencegraner@kinsta.com>
2022-02-13 15:57:32 +0100

    add server side props to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
+import { GetServerSideProps } from "next"
+
 const Home = (): JSX.Element => {
   return (
     <div>index</div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      isSession: true,
+    },
+    redirect: {
+      destination: '/anna'
+    }
+  }
 }
 
 export default Home
